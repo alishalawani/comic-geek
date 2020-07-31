ba10d30f2557b36bf2316fdeb4df4e0f78316331
alishalawani <alishalawani@gmail.com>
2020-07-30 20:54:39 -0500

    added a search error message to the search component

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,63 +1,81 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
+let found = false
 class Search extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            search: '',
+			search: '',
             submitted: false,
 		};
 	}
 
 	handleInputChange = (event) => {
-        
 		this.setState({ search: event.target.value, submitted: false });
-    };
-    
-    handleSubmit = (event) =>{
-event.preventDefault();
-this.setState({submitted: true})
+	};
 
-
-    }
+	handleSubmit = (event) => {
+		event.preventDefault();
+		this.setState({ submitted: true });
+	};
 
 	render() {
-let characterDisplay;
-        if(this.state.submitted === true){
-		let filteredCharacters = this.props.characters.filter((character) => {
-			return character.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1 && this.state.search && character.name.toLowerCase().charAt(0) === this.state.search.toLowerCase().charAt(0)
-		});
-		
-		characterDisplay = filteredCharacters.map((character, index) => {
-			if (character.biography.publisher === "DC Comics") {
-				
-					return <Link to={`/dc-characters/:${character.name}`} key={index}>
-						<div className='dc-character'>
-							<img
-								src={character.image.url}
-								alt={character.name}
-								className='dc-char'
-							/>
-							<h3>{character.name}</h3>
-						</div>
-					</Link>
-			
-            } 
-            if (character.biography.publisher === 'Marvel Comics') {
-				
-					return <Link to={`/marvel-characters/:${character.name}`} key={index}>
-						<div className='dc-character'>
-							<img
-								src={character.image.url}
-								alt={character.name}
-								className='dc-char'
-							/>
-							<h3>{character.name}</h3>
-						</div>
-					</Link>
-				
-			}
-		});}
+		let characterDisplay;
+		if (this.state.submitted === true) {
+			let filteredCharacters = this.props.characters.filter((character) => {
+				if (
+					character.name
+						.toLowerCase()
+						.indexOf(this.state.search.toLowerCase()) !== -1 &&
+					this.state.search &&
+					character.name.toLowerCase().charAt(0) ===
+						this.state.search.toLowerCase().charAt(0)
+				) {
+                    found = true
+				}else{
+                    found = false
+                }
+				return (
+					character.name
+						.toLowerCase()
+						.indexOf(this.state.search.toLowerCase()) !== -1 &&
+					this.state.search &&
+					character.name.toLowerCase().charAt(0) ===
+						this.state.search.toLowerCase().charAt(0)
+				);
+			});
+
+			characterDisplay = filteredCharacters.map((character, index) => {
+				if (character.biography.publisher === 'DC Comics') {
+					return (
+						<Link to={`/dc-characters/:${character.name}`} key={index}>
+							<div className='dc-character'>
+								<img
+									src={character.image.url}
+									alt={character.name}
+									className='dc-char'
+								/>
+								<h3>{character.name}</h3>
+							</div>
+						</Link>
+					);
+				} else if (character.biography.publisher === 'Marvel Comics') {
+					return (
+						<Link to={`/marvel-characters/:${character.name}`} key={index}>
+							<div className='dc-character'>
+								<img
+									src={character.image.url}
+									alt={character.name}
+									className='dc-char'
+								/>
+								<h3>{character.name}</h3>
+							</div>
+						</Link>
+					);
+				}
+			});
+		}
 
 		return (
 			<div>
@@ -70,7 +88,9 @@ let characterDisplay;
 						onChange={this.handleInputChange}></input>
 					<button type='submit'>Search</button>
 				</form>
-                {characterDisplay}
+				{this.state.submitted && <h2>Your Search result:</h2>}
+                {(!found && this.state.submitted) && <h3 className='search-error'>Sorry, the character you are searched for isn't available, check if you typed the name correctly or try searching for a different character </h3>}
+				{characterDisplay}
 			</div>
 		);
 	}
