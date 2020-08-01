5c7f5a97e910d0ff530b57904ab186da321a3a46
alishalawani <alishalawani@gmail.com>
2020-07-31 19:44:06 -0500

    edited homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Home.css'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import { CardColumns, Card, Row, Col } from 'react-bootstrap';
 
 function Home(props) {
     return (
-			<div>
-				<img src='https://studybreaks.com/wp-content/uploads/2016/09/dc-vs-marvel.jpg' alt='Marvel and DC'/>
-			</div>
+			<Container>
+				<Image
+					fluid
+					src='https://studybreaks.com/wp-content/uploads/2016/09/dc-vs-marvel.jpg'
+					alt='Marvel and DC'
+				/>
+
+				<Row>
+					<Col>
+					
+						<Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTYNp9w-Vdj1uCg9vvIJ_2FGrJEdBzzMm6qeQ&usqp=CAU' />
+					</Col>
+					<Col md='auto'>
+						<Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTYNp9w-Vdj1uCg9vvIJ_2FGrJEdBzzMm6qeQ&usqp=CAU' className='home-img'/>
+					</Col>
+				</Row>
+			</Container>
 		);
 }
 
