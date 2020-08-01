ed6a03a171dc655988a2ec0b30487d92403a2cb0
alishalawani <alishalawani@gmail.com>
2020-08-01 15:43:38 -0500

    styled the marvelDisplay component view

--- src/Components/Marvel/MarvelDisplay.js
+++ src/Components/Marvel/MarvelDisplay.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './MarvelDisplay.css';
+import { Container, Row, Col, Card, Image } from 'react-bootstrap';
 class MarvelDisplay extends Component {
 	render() {
 		let characterInfo;
@@ -8,44 +9,60 @@ class MarvelDisplay extends Component {
 			// console.log("character name -" + character1.name);
 			if (`:${character.name}` === this.props.match.params.name) {
 				characterInfo = (
-					<div>
-						<h1>{character.name}</h1>
-						<img
-							src={character.image.url}
-							alt={character.name}
-							className='marvel-image'
-						/>
-						<div className='biography'>
-							<h2>Biography:</h2>
-							<p>
+					<div className='marvel-container'>
+						<div className='profile'>
+							<h1 className='marvel-char-name'>{character.name}</h1>
+
+							<img
+								src={character.image.url}
+								alt={character.name}
+								className='marvel-image'
+							/>
+						</div>
+						<div className='info'>
+							<h2 className='bio'>Biography</h2>
+							<p className='name'>
 								<span className='full-name'>Full Name:</span>{' '}
 								{character.biography['full-name']}
 							</p>
-							<h3>Alias(es)</h3>
+							<h5 className='bio'>Alias(es)</h5>
 							<ul>
 								{character.biography.aliases.map((alias, index) => {
 									return <li key={index}>{alias}</li>;
 								})}
 							</ul>
-                            <h4>Place of Birth: {character.biography['place-of-birth']}</h4>
-                            <h4>Alter Ego(s)</h4>
-                            <p>{character.biography['alter-egos']}</p>
-                            <h2>Appearance</h2>
-                            <p>Height: {character.appearance.height[0]}</p>
-                            <p>Gender: {character.appearance.gender}</p>
-                            <p>Eye color: {character.appearance['eye-color']}</p>
-                            <p>Hair Color: {character.appearance['hair-color']}</p>
+							<p>
+								<span className='bio'>Place of Birth:</span>{' '}
+								{character.biography['place-of-birth']}
+							</p>
+							<p>
+								<span className='bio'>Alter Ego(s):</span>{' '}
+								{character.biography['alter-egos']}
+							</p>
+							<h3>Appearance:</h3>
+							<p>Height: {character.appearance.height[0]}</p>
+							<p>Gender: {character.appearance.gender}</p>
+							<p>Eye color: {character.appearance['eye-color']}</p>
+							<p>Hair Color: {character.appearance['hair-color']}</p>
 							<h3>Power Stats:</h3>
 							<p>Combat: {character.powerstats.combat}</p>
 							<p>Durability: {character.powerstats.durability}</p>
-                            <p>Occupation: {character.work.occupation}</p>
+							<p>
+								<span className='occupation'>Occupation:</span>{' '}
+								{character.work.occupation}
+							</p>
 						</div>
 					</div>
 				);
 			}
 		});
 
-		return <div>{characterInfo}</div>;
+		return (
+			<Container className='main-marvel-container'>
+				<hr className='hr-marvel-display' />
+				{characterInfo}
+			</Container>
+		);
 	}
 }
 
